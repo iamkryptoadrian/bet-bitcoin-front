75b10c69a9e3f33a45f6476b89f497d6ba98955d
Its_abdulshaikh <iamkryptoadrian@gmail.com>
2023-10-04 17:51:40 +0530

    UDPAET API

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 
 const config = {
-    apiKey : "http://localhost:3000/api/",//process.env.REACT_APP_API_KEY,
+    apiKey : "https://api.bet-bitcoin.ai/api/",//process.env.REACT_APP_API_KEY,
     baseUrl:'/',
     projectId:'4de52e20ff4ec6762efc2e3ec4fb66d7',
 
